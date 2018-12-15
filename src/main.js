2d741255c1a2fd41caaa89e92a2c0f493f35b7e6
buyuanlee <buyuanlee@gmail.com>
2018-12-16 00:02:01 +0800

    调整文件，增加默认样式并引入

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import './assets/reset.scss'
+
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+  router
+}).$mount('#app');
